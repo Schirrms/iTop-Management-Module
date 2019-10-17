e22b55b1817bc5e6e9b4bf9cd5c49bb30b969042
Schirrms <schirrms@schirrms.net>
2019-10-17 22:33:14 +0200

    French translation (0.1.0.12)

--- schirrms-management-module/en.dict.schirrms-management-module.php
+++ schirrms-management-module/en.dict.schirrms-management-module.php
@@ -9,13 +9,14 @@
 Dict::Add('EN US', 'English', 'English', array(
 	// Dictionary entries go here
 	'Class:ManagementModule' => 'Management Module',
+	'Class:ManagementModule+' => 'An embedded card or a dedicated module for this device, like an iLO/Drake card for Servers...',
 	'Class:ManagementModule/Attribute:connectableci_id' => 'Device hosting this management module',
 	'Class:ManagementModule/Attribute:connectableci_name' => 'Device hosting this management module name',
 	'Class:ManagementModule/Attribute:iosversion_id' => 'Firmware/Software version',
 	'Class:ManagementModule/Attribute:iosversion_name' => 'Firmware/Software version Name',
 	'Class:ConnectableCI/Attribute:managementmodule_list' => 'Management module(s)',
 	'Class:ConnectableCI/Attribute:managementmodule_list+' => 'Embedded or dedicated management module(s) of this device',
-	'Class:ConnectableCI/Attribute:ManagementModuleRedundancy' => 'Redundancy mode',
+	'Class:ConnectableCI/Attribute:ManagementModuleRedundancy' => 'Redundancy mode (normally set at 0, to allow the loss of the management)',
 	'Class:ConnectableCI/Attribute:ManagementModuleRedundancy/percent' => '%2$s is up if at least %1$s %% of Management Modules are up',
 	'Class:ConnectableCI/Attribute:ManagementModuleRedundancy/count' => '%2$s is up if at least %1$s Management Modules are up',
 	'Class:ConnectableCI/Attribute:ManagementModuleRedundancy+' => 'This should normally always be set to \'0%%\' as the device probably can work without it\'s management module',
--- schirrms-management-module/module.schirrms-management-module.php
+++ schirrms-management-module/module.schirrms-management-module.php
@@ -49,5 +49,4 @@ SetupWebPage::AddModule(
 	)
 );
 
-
 ?>
