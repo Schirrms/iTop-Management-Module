5df2965e364e68eb9b3d52e6cff8c9ca8cbb2b08
Schirrms <schirrms@schirrms.net>
2019-10-17 21:57:21 +0200

    some improvements (0.1.0.9)

--- schirrms-management-module/en.dict.schirrms-management-module.php
+++ schirrms-management-module/en.dict.schirrms-management-module.php
@@ -8,5 +8,12 @@
 
 Dict::Add('EN US', 'English', 'English', array(
 	// Dictionary entries go here
+	'Class:ManagementModule' => 'Management Module',
+	'Class:ManagementModule/Attribute:connectableci_id' => 'Device hosting this management module',
+	'Class:ManagementModule/Attribute:iosversion_id' => 'Firmware/Software version',
+	'Class:ConnectableCI/Attribute:managementmodule_list' => 'Management module(s)',
+	'Class:ConnectableCI/Attribute:managementmodule_list+' => 'Embedded or dedicated management module(s) of this device',
+	'Class:ConnectableCI/Attribute:ManagementModuleRedundancy' => 'Redundancy mode',
+	'Class:ConnectableCI/Attribute:ManagementModuleRedundancy+' => 'This should normally always be set to \'0%\' as the device probably can work without it\'s management module',
 ));
 ?>
