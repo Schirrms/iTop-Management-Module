37443c84e870545f0b09f600b286ef067de06084
Schirrms <schirrms@schirrms.net>
2019-10-17 22:14:34 +0200

    Text improvement(0.1.0.11)

--- schirrms-management-module/en.dict.schirrms-management-module.php
+++ schirrms-management-module/en.dict.schirrms-management-module.php
@@ -2,7 +2,7 @@
 /**
  * Localized data
  *
- * @copyright   Copyright (C) 2013 XXXXX
+ * @copyright   Copyright (C) 2019 Schirrms
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
@@ -10,12 +10,14 @@ Dict::Add('EN US', 'English', 'English', array(
 	// Dictionary entries go here
 	'Class:ManagementModule' => 'Management Module',
 	'Class:ManagementModule/Attribute:connectableci_id' => 'Device hosting this management module',
+	'Class:ManagementModule/Attribute:connectableci_name' => 'Device hosting this management module name',
 	'Class:ManagementModule/Attribute:iosversion_id' => 'Firmware/Software version',
+	'Class:ManagementModule/Attribute:iosversion_name' => 'Firmware/Software version Name',
 	'Class:ConnectableCI/Attribute:managementmodule_list' => 'Management module(s)',
 	'Class:ConnectableCI/Attribute:managementmodule_list+' => 'Embedded or dedicated management module(s) of this device',
 	'Class:ConnectableCI/Attribute:ManagementModuleRedundancy' => 'Redundancy mode',
-	'Class:ConnectableCI/Attribute:ManagementModuleRedundancy/percent' => '%2$% is up if at least %1$% %% of Management Modules are up',
-	'Class:ConnectableCI/Attribute:ManagementModuleRedundancy/count' => '%2$% is up if at least %1$% Management Modules are up',
+	'Class:ConnectableCI/Attribute:ManagementModuleRedundancy/percent' => '%2$s is up if at least %1$s %% of Management Modules are up',
+	'Class:ConnectableCI/Attribute:ManagementModuleRedundancy/count' => '%2$s is up if at least %1$s Management Modules are up',
 	'Class:ConnectableCI/Attribute:ManagementModuleRedundancy+' => 'This should normally always be set to \'0%%\' as the device probably can work without it\'s management module',
 ));
 ?>
